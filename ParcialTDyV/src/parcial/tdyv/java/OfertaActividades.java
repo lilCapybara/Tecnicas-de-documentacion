008720d070602b0b81a31764db1b90b21535aaeb
Sebastian Ortega <sortega@alumnos.exa.unicen.edu.ar>
2024-10-21 19:00:17 -0300

    Agregado codigo de parcial

---
+++ ParcialTDyV/src/parcial/tdyv/java/OfertaActividades.java
@@ -0,0 +1,26 @@
+package parcial.tdyv.java;
+
+import java.util.ArrayList;
+
+public class OfertaActividades {
+	static ArrayList<Actividad> nomina;
+	
+	static void NuevaActividad(Actividad act) {
+		if(nomina==null) nomina = new ArrayList<Actividad>();
+		if(nomina.indexOf(act)<0) nomina.add(act);
+	}
+	
+	static void BorrarActividad(Actividad act) {
+		
+		if(nomina.indexOf(act)<0) nomina.remove(act);
+	}
+	
+	static int CantidadActividades() {
+		return nomina.size();
+	}
+
+	static ArrayList<Actividad>  GetNomina() {
+	return nomina;
+	}
+	
+}
